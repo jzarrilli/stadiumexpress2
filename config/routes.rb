59982224a16a985b78d0378cf7bf5a36ac75d875
Lindsey Eatough <lindsey.eatough@gmail.com>
2011-03-22 14:27:04 -0400

    Venues to Stand to Store complete

--- config/routes.rb
+++ config/routes.rb
@@ -85,7 +85,7 @@ Depot::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
-  root :to => 'store#index', :as => 'store'
+  root :to => 'venues#index'
 
   # ...
 
