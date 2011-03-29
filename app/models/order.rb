b5bfe46b0a18438db08145fd479b70572731dfec
Lindsey Eatough <lindsey.eatough@gmail.com>
2011-03-29 00:16:38 -0400

    Fixing the payment method drop down options to incorporate cash and paypal options

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ActiveRecord::Base
   has_many :line_items, :dependent => :destroy
-  PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order" ]
+  PAYMENT_TYPES = [ "Cash", "Check", "Credit card", "PayPal" ]
   
   validates :name, :address, :email, :pay_type, :presence => true
   validates :pay_type, :inclusion => PAYMENT_TYPES
