b216f9c69c2ef76c7a7927713f4ab2207b9c3030
Lindsey Eatough <lindsey.eatough@gmail.com>
2011-03-31 14:48:31 -0400

    Fix so not prompted to log in when user accessing cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,5 @@
 class CartsController < ApplicationController
-  skip_before_filter :authorize, :only => [:create, :update, :destroy]
+  skip_before_filter :authorize# , :only => [:create, :update, :destroy]
  
   # GET /carts
   # GET /carts.xml
