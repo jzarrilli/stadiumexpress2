0dcfeb0f82cab962bc732c38a8fa5f38a233cbb1
Lindsey Eatough <lindsey.eatough@gmail.com>
2011-03-30 20:59:03 -0400

    Fixed routing so that the cart is it's own page and links to: order more, empty cart and proceed to checkout

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -48,8 +48,8 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to(store_index_url) }
-        format.js	{ @current_item = @line_item }
+        format.html { redirect_to(@line_item.cart) }
+        # format.js { @current_item = @line_item }
         format.xml  { render :xml => @line_item, :status => :created, :location => @line_item }
       else
         format.html { render :action => "new" }
