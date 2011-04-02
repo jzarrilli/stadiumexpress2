8f60780669ad21db1591b98f7423cb9d1b8388ac
Jordan Zarrilli <jzarrilli@gmail.com>
2011-04-02 19:32:25 -0400

    cart

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,57 @@
-#---
-# Excerpted from "Agile Web Development with Rails, 4rd Ed.",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material, 
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose. 
-# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
-#---
-# encoding: utf-8
-
-
-User.create(:name => 'admin', :password => 'password')
+  # 
+  # Product.delete_all
+  # Product.create(:title => 'Philly Pretzel',
+  #   :description => 
+  #     %{<p>
+  #         This is a Philly Pretzel.
+  #       </p>},
+  #   :image_url =>   '/images/pretzel.jpg',    
+  #   :price => 1.50)
+  #   
+  # # . . .
+  # Product.create(:title => 'Can of Coca Cola',
+  #   :description =>
+  #     %{<p>
+  #         This is a can of Coca Cola.
+  #       </p>},
+  #   :image_url => '/images/cocacola.jpg',
+  #   :price => 1.00)
+  # # . . .
+  # 
+  # Product.create(:title => 'Can of Sprite',
+  #    :description =>
+  #      %{<p>
+  #          This is a can of Sprite.
+  #        </p>},
+  #    :image_url => '/images/sprite.jpg',
+  #    :price => 1.00)
+  #  # . . .
+  # 
+  # Product.create(:title => 'Bottle of Water',
+  #   :description => 
+  #     %{<p>
+  #         This is a bottle of water.
+  #       </p>},
+  #   :image_url => '/images/water.jpg',
+  #   :price => 1.00)
+  # 
+  # Venue.create(:name => 'Meiklejohn Stadim (you are here!)')
+  # 
+  # Venue.create(:name => 'Franklin Field')
+  # 
+  # Venue.create(:name => 'Palestra')
+  # 
+  # Stand.create(:name => 'Concession Stand', :location => 'Entrance Gate')
+  # 
+  # User.create(:name => 'Lindsey', :password => 'lindsey')
+  # 
+  # User.create(:name => 'Jordan', :password => 'jordan')
+  # 
+  # User.create(:name => 'Tosin', :password => 'tosin')
+  # 
+  # User.create(:name => 'Adam', :password => 'adam')
+  # 
+  # User.create(:name => 'Admin', :password => 'password')
+  #   
+  # 
+  # 
