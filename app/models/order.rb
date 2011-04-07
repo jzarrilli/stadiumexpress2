56018427339f295782a180bf3b413c856307d2e2
Jordan Zarrilli <jzarrilli@gmail.com>
2011-04-07 11:33:46 -0400

    figuring out price

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,18 @@
 class Order < ActiveRecord::Base
+  belongs_to :cart
+  belongs_to :line_item
+  belongs_to :product
+  
   has_many :line_items, :dependent => :destroy
   PAYMENT_TYPES = [ "Cash", "Check", "Credit card", "PayPal" ]
   
   validates :name, :address, :email, :pay_type, :presence => true
   validates :pay_type, :inclusion => PAYMENT_TYPES
+  
+  def total_price(cart)
+    cart.line_items.to_a.sum { |item| item.total_price }
+  end
+ 
   
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
@@ -12,6 +21,7 @@ class Order < ActiveRecord::Base
     end
   end
     
+
 
   
 end
