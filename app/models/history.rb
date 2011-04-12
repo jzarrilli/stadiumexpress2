63bb450b2785315194e0d031d9f9396703666297
Lindsey Eatough <lindsey.eatough@gmail.com>
2011-04-11 23:24:10 -0400

    updating history page and formatting

--- app/models/history.rb
+++ app/models/history.rb
@@ -1,7 +1,14 @@
 class History < ActiveRecord::Base
-  belongs_to :order
   belongs_to :product
   belongs_to :cart
+  
+  has_many :line_items
+  has_many :orders, :through => :line_items
+  
+  validates :product_id, :presence => true
+  validates :cart_id, :presence => true,
+  validates :name, :address, :email, :pay_type, :presence => true
+
   
   def total_price
     product.price * quantity
