5af507d284d5ab3b1c173ca854e99cfb572f1e22
Jordan Zarrilli <jzarrilli@gmail.com>
2011-04-19 19:31:48 -0400

    pagnate

--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -4,6 +4,8 @@ class HistoryController < ApplicationController
   def index
     @line_items = LineItem.where(:status => [LineItem::VERIFIED, LineItem::PICKED_UP])
     @line_items = @line_items.order('created_at desc')
+    @line_items = LineItem.paginate :page=>params[:page], :order=>'created_at desc',
+    :per_page => 10
     
     respond_to do |format|
       format.html # index.html.erb
--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -9,6 +9,8 @@ class LineItemsController < ApplicationController
   def index
     @line_items = LineItem.where(:status => [LineItem::VERIFIED, LineItem::FILLED])
     @line_items = @line_items.order('created_at desc')
+    @line_items = LineItem.paginate :page=>params[:page], :order=>'created_at desc',
+    :per_page => 10
     
     respond_to do |format|
       format.html # index.html.erb
